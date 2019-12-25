fd45fe4d083581871cc1613c7c1724dedfcb2c46
Chatchai Saratakij <CSaratakij@gmail.com>
2019-12-25 14:24:07 +0700

    Add : Cone target

---
+++ Assets/Scripts/Orbit.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    [SerializeField]
+    Transform pivot;
+
+    [SerializeField]
+    float orbitSpeed = 30.0f;
+
+    void LateUpdate()
+    {
+        transform.RotateAround(pivot.position, Vector3.up, orbitSpeed * Time.deltaTime);
+    }
+}
